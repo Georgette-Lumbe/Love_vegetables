f413d84fdfc03384673fa76017cc882e74edec35
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-06-20 13:51:46 +0000

    Create an array and pass number, questions, options and answers

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+//Creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: 'Which of these vegetables was the first to be grown in space?',
+        answer: "Potato",
+        option: [
+            "Radish",
+            "Peas",
+            "Swede",
+            "Potato"
+        ]
+    },
+    {
+        numb: 2,
+        question: 'What is the most popular vegetable in the United States?',
+        answer: "Brocoli",
+        option: [
+            "Brussels Sprouts",
+            "Brocoli",
+            "Artichoke",
+            "Zucchini"
+        ]
+    },
+    {
+        numb: 3,
+        question: 'Where did Swiss Chard originate?',
+        answer: "Spain",
+        option: [
+            "Spain",
+            "Switzerland",
+            "Ethiopia",
+            "Norway"
+        ]
+    },
+    {
+        numb: 4,
+        question: 'Which of the following statements about carrots is true?',
+        answer: "None of the above",
+        option: [
+            "They help you see in the dark",
+            "They originated in the United States",
+            "They are made up of 96% water",
+            "None of the above"
+        ]
+    },
+    {
+        numb: 5,
+        question: 'Which of the following vegetables has an aniseed flavour?',
+        answer: "Fennel",
+        option: [
+            "Asparagus",
+            "Pak Choi",
+            "Fennel",
+            "Garlic"
+        ]
+    },
+]
