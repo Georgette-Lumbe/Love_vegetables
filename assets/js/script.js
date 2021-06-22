3e8bd29ec21bcbadd48f1979f5c7ee5ef667f9d0
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-06-22 01:04:33 +0000

    Put onclick event when the next button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,17 +20,29 @@ exit_btn.onclick = ()=>{
 continue_btn.onclick = ()=>{
     rules_box.classList.remove("activeRule"); //hide the instructions box
     quiz_box.classList.add("activeQuiz"); //show the quiz box
-    showQuestions(0)
+    showQuestions(0);
 
 }
 
 let que_count = 0;
 
+const next_btn = quiz_box.querySelector('.next_btn');
+
+//If Next Button clicked
+next_btn.onclick = ()=> {
+    if(que_count < questions.length -1){
+        que_count++;
+        showQuestions(que_count);
+    } else {
+        console.log("Completed");
+    }
+}
+
 //Getting questions and options from array
 function showQuestions(index){
     const questions_text = document.querySelector('.questions_text');
     const option_list = document.querySelector('.option_list');
-    let que_tag = '<span>'+ questions[index].question +'</span>';
+    let que_tag = '<span>'+ questions[index].numb + '. ' + questions[index].question +'</span>';
     let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
                      + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
                      + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
